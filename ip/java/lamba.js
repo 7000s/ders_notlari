f2736ef883172a67d0a3bcf464d73d4f8d3fb7f7
7000s <damyo7013@gmail.com>
2025-01-13 15:29:47 +0300

    1yeni

--- ip/java/lamba.js
+++ ip/java/lamba.js
@@ -1,8 +1,12 @@
 let lamba = document.querySelector("img")
+var baslik = document.getElementById("baslik")
 console.log(lamba)
-
+console.log(baslik)
 function ac(){
     lamba.setAttribute("src","../img/acik.png")
+    //baslik.setAttribute("class","baslik3")
+    console.log(baslik.classList)
+    baslik.classList.add("baslik3")
 }
 function kapa(){
     lamba.setAttribute("src","../img/kapali.png")
